3103a11a1a8a5a4f87595da131a27bee23f88e4f
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-03-09 16:02:33 -0300

    feat: estilização incial do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
 import { HeaderContainer } from './styles'
 
+import LoaLogo from '../../assets/LOA_LOGO.png'
+import { Timer, Scroll } from '@phosphor-icons/react'
+import { NavLink } from 'react-router-dom'
+
 export default function Header() {
   return (
     <HeaderContainer>
+      <img src={LoaLogo} alt="Logo aplicação" />
       <nav>
-        <a href="">timer</a>
-      </nav>
-      <nav>
-        <a href="">menu</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} color="#E1E1E6" />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} color="#E1E1E6" />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
