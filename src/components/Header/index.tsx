2329adf791ce1f118248307a164090042255931c
Alessandro Hugen <alessandro.hugen@selectinvestimentos.com>
2023-03-09 15:31:58 -0300

    feat: cria e adiciona container estilizados para layout e header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer } from './styles'
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <nav>
+        <a href="">timer</a>
+      </nav>
+      <nav>
+        <a href="">menu</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
